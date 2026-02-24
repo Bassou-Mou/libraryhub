cb2f39fe58f5db97318259e8e24fbdd401ffbc4e
Bassou Mou <bassoumouacha@gmail.com>
2026-02-24 15:56:03 +0100

    feat: add DTO layer for request and response handling

---
+++ src/main/java/com/library/libraryhub/dto/request/RegisterRequest.java
@@ -0,0 +1,27 @@
+package com.library.libraryhub.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    private String firstName;
+
+    @NotBlank(message = "Le nom est obligatoire")
+    private String lastName;
+
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "Format email invalide")
+    private String email;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    @Size(min = 6, message = "Le mot de passe doit faire au moins 6 caractères")
+    private String password;
+
+    @NotBlank(message = "Le numéro étudiant est obligatoire")
+    private String studentNumber;
+}
